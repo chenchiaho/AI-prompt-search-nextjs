04b7c43061dffac9c766c22801c498b8f425f824
chenchiaho <chenchiaho@gmail.com>
2023-06-06 22:38:58 +0800

    add user schema

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "models/User";
+import { connectToDB } from "utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -12,9 +15,30 @@ const handler = NextAuth({
     async session({ session }) {
 
     },
-    async signIn(user, account, profile, credentials) { }
-  }
+    async signIn(user, account, profile, credentials) {
+      try {
+        await connectToDB()
 
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email })
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    }
+
+  }
 })
 
 export { handler as GET, handler as POST }
